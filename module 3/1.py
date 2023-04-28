90bb13d57c9dbdc2cc116913e360ef1fd7f85fa2
jainilpatel14012002 <jainil14012002@gmail.com>
2023-04-28 16:30:05 +0530

    module3

---
+++ module 3/1.py
@@ -0,0 +1,5 @@
+# list = list can store any type of data value
+# list is mix type of data(multiple data types)
+l1 = [1,1,2,4,5,6]
+z = l1[::-1]
+print(z)
---
+++ module 3/2.py
@@ -0,0 +1,8 @@
+# remove last object from the list
+l1 = [5,4,3,2,1]
+l2 = [1,2,3,4,5]
+
+print(id(l1))
+print(id(l2))
+
+
